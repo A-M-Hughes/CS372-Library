6107b7d90a8e338206cd9684f0c896dbd5227813
lukebushur <luke7ajb@gmail.com>
2023-11-16 02:08:42 -0500

    Added back verifyToken middleware to book API routes

--- server/routes/booksApiRoutes.js
+++ server/routes/booksApiRoutes.js
@@ -10,23 +10,23 @@ const router = express.Router();
 //Routes
 
 //Get featured books.
-router.get('/featured', /*verifyToken,*/ booksApiController.featured);
+router.get('/featured', verifyToken, booksApiController.featured);
 
 //Get work information from a specified Open Library ID (OLID).
 //The ID MUST end with a 'W'.
-router.get('/works/:id', /*verifyToken,*/ booksApiController.workInfo);
+router.get('/works/:id', verifyToken, booksApiController.workInfo);
 
 //Get information about 10 editions of a specified work.
 //The ID MUST end with a 'W'.
-router.get('/editions/:id', /*verifyToken,*/ booksApiController.workEditionInfo);
-router.get('/editions/:id/:page', /*verifyToken,*/ booksApiController.workEditionInfo);
+router.get('/editions/:id', verifyToken, booksApiController.workEditionInfo);
+router.get('/editions/:id/:page', verifyToken, booksApiController.workEditionInfo);
 
 //Get book information from a specified Open Library ID (OLID).
 //The ID MUST end with an 'M'.
-router.get('/books/:id', /*verifyToken,*/ booksApiController.bookInfo);
+router.get('/books/:id', verifyToken, booksApiController.bookInfo);
 
 //Search OpenLibrary for a book. The query can be a book title, ISBN-10, ISBN-13, or OLID.
-router.get('/searchBooks/:query', /*verifyToken,*/ booksApiController.searchBooks);
-router.get('/searchBooks/:query/:page', /*verifyToken,*/ booksApiController.searchBooks);
+router.get('/searchBooks/:query', verifyToken, booksApiController.searchBooks);
+router.get('/searchBooks/:query/:page', verifyToken, booksApiController.searchBooks);
 
 module.exports = router;
