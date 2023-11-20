811642934eefbf9448e5d5371f795bee89f2d62f
Sean Tierney <stierney0505@gmail.com>
2023-11-20 18:20:34 -0500

    Including CORS

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ const authRoutes = require('./routes/authRoutes');
 const booksApiRoutes = require('./routes/booksApiRoutes');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+const cors = require('cors');
 
 // MIDDLEWARE:
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 
 // ROUTES:
 
