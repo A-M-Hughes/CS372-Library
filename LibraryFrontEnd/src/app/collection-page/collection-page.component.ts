6268544d3c0fd63dca02b1ac13946a3cba0d7294
Adam Hughes <hugham08@pfw.edu>
2023-11-29 18:00:10 -0500

    basic collections page generation and base code

--- LibraryFrontEnd/src/app/collection-page/collection-page.component.ts
+++ LibraryFrontEnd/src/app/collection-page/collection-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookCollectionService } from 'src/controllers/book-collection-controller/book-collection.service';
 
 @Component({
   selector: 'app-collection-page',
   templateUrl: './collection-page.component.html',
   styleUrls: ['./collection-page.component.css']
 })
-export class CollectionPageComponent {
+export class CollectionPageComponent implements OnInit{
 
+  constructor(private booksCollectionService: BookCollectionService) {}
+  books: any;
+
+  ngOnInit(): void {
+    this.booksCollectionService.getAllBooksFromCollection().subscribe((data) => {
+      this.books = data;
+    });
+  }
 }
--- LibraryFrontEnd/src/controllers/book-collection-controller/book-collection.service.ts
+++ LibraryFrontEnd/src/controllers/book-collection-controller/book-collection.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../auth-controller/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookCollectionService {
+  private apiUrl = environment.apiurl
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+
+
+  addBookToCollection(bookData: any) {
+    const headers = this.authService.getHeaders();
+    return this.http.post(`${this.apiUrl}/collections/addBook`, bookData, { headers });
+  }
+
+  getAllBooksFromCollection() {
+    const headers = this.authService.getHeaders();
+    return this.http.get(`${this.apiUrl}/collections/getBooks`, { headers });
+  }
+
+  deleteBookFromCollection(bookID: string) {
+    const headers = this.authService.getHeaders();
+    return this.http.delete(`${this.apiUrl}/collections/deleteBook`, { headers, body: { bookID } });
+  }
+
 }
