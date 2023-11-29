3575c5b80723f393e87b73474fbdf7975b50d3ba
Adam Hughes <hugham08@pfw.edu>
2023-11-29 17:14:22 -0500

    organized services and generated collection page

--- LibraryFrontEnd/src/app/search-page/search-page.component.ts
+++ LibraryFrontEnd/src/app/search-page/search-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BookSearchService } from '../book-search.service';
+import { BookSearchService } from '../../controllers/book-search-controller/book-search.service';
 
 @Component({
   selector: 'app-search-page',
