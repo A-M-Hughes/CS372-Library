2a1ce114f9a30341722e557c137da947180d2d1d
Adam Hughes <hugham08@pfw.edu>
2023-11-29 18:36:18 -0500

    add book to collection functionality

--- LibraryFrontEnd/src/controllers/book-collection-controller/book-collection.service.ts
+++ LibraryFrontEnd/src/controllers/book-collection-controller/book-collection.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../auth-controller/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +14,17 @@ export class BookCollectionService {
 
 
 
-  addBookToCollection(bookData: any) {
+  addBookToCollection(bookData: any): Observable<any> {
     const headers = this.authService.getHeaders();
     return this.http.post(`${this.apiUrl}/collections/addBook`, bookData, { headers });
   }
 
-  getAllBooksFromCollection() {
+  getAllBooksFromCollection(): Observable<any> {
     const headers = this.authService.getHeaders();
     return this.http.get(`${this.apiUrl}/collections/getBooks`, { headers });
   }
 
-  deleteBookFromCollection(bookID: string) {
+  deleteBookFromCollection(bookID: string): Observable<any> {
     const headers = this.authService.getHeaders();
     return this.http.delete(`${this.apiUrl}/collections/deleteBook`, { headers, body: { bookID } });
   }
