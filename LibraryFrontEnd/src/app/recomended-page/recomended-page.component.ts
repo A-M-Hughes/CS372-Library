0297ae7dff44f24b09c9a5661cd9f241f97bdc7d
Adam Hughes <hugham08@pfw.edu>
2023-12-01 20:15:54 -0500

    generated component and service

---
+++ LibraryFrontEnd/src/app/recomended-page/recomended-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recomended-page',
+  templateUrl: './recomended-page.component.html',
+  styleUrls: ['./recomended-page.component.css']
+})
+export class RecomendedPageComponent {
+
+}
